8b8bb1f401ec40d1ac9f09f4f45e827ca97399c9
Sergio Escudero <sescuderon@gmail.com>
2026-04-18 19:22:40 -0700

    agrego panel de admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,7 @@ import Noticias from './pages/Noticias';
 import NoticiaDetalle from './pages/NoticiaDetalle';
 import Login from './pages/Login';
 import EmpresaDetalle from './pages/EmpresaDetalle';
-import AdminUsuarios from './pages/admin/AdminUsuarios';
-import AdminEmpresa from './pages/admin/AdminEmpresa';
+import Admin from './pages/Admin';
 
 export default function App() {
   return (
@@ -18,8 +17,7 @@ export default function App() {
         <Route path="/noticias/:id" element={<NoticiaDetalle />} />
         <Route path="/login" element={<Login />} />
         <Route path="/empresa/:id" element={<EmpresaDetalle />} />
-        <Route path="/admin/usuarios" element={<AdminUsuarios />} />
-        <Route path="/admin/empresa" element={<AdminEmpresa />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
