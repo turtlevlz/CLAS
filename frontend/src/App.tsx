8b595be69ea5cf676ec85e8d716d753dfaf4f86c
santiago <a01255853@tec.mx>
2026-04-29 23:41:12 -0700

    fix typo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ import ForgotPswd from './pages/ForgotPswd';
 import NuevaEmpresa from './pages/NuevaEmpresa';
 import EditarEmpresa from './pages/EditarEmpresa';
 import ErrorPage from './pages/ErrorPage';
-import Membresias from './pages/membresias';
+import Membresias from './pages/Membresias';
 
 const RutaProtegida = ({ children }: { children: any }) => {
   const { usuarioActual } = useAuth();
