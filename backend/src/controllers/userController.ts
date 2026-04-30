4103d04a6d54fc7347cf757d8e11709e5a52a01c
carlosbadillar <A01255765@tec.mx>
2026-04-29 18:15:18 -0700

    fix: se actualizo userController con validaciones de zod

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,11 +2,14 @@ import { Request, Response } from "express";
 import { User } from "../models/User";
 import { Empresa } from "../models/Empresa";
 import { Op } from "sequelize";
+import { z } from "zod";    
 import bcrypt from "bcrypt";
 import { Role } from "../models/Role";
 
 export const createUser = async (req: Request, res: Response) => {
+
     try {
+
         const {
             nombre_usuario,
             contrasena,
@@ -16,19 +19,30 @@ export const createUser = async (req: Request, res: Response) => {
         } = req.body;
 
         const rolId = Number(rol_id);
-        const empresaId = (empresa_id === undefined || empresa_id === null || empresa_id === "") ? null : Number(empresa_id);
+        const empresaId = empresa_id === undefined || empresa_id === null || empresa_id === "" ? null : Number(empresa_id);
 
         if (!nombre_usuario || String(nombre_usuario).trim() === "") {
-            return res.status(400).json({ message: "El nombre de usuario es obligatorio" });
+            return res.status(400).json({
+                message: "El nombre de usuario es obligatorio"
+            });
         }
+
         if (!correo_electronico || String(correo_electronico).trim() === "") {
-            return res.status(400).json({ message: "El correo electrónico es obligatorio" });
+            return res.status(400).json({
+                message: "El correo electronico es obligatorio"
+            });
         }
+
         if (!contrasena || String(contrasena).trim() === "") {
-            return res.status(400).json({ message: "La contraseña es obligatoria" });
+            return res.status(400).json({
+                message: "La contrasena es obligatoria"
+            });
         }
+
         if (isNaN(rolId)) {
-            return res.status(400).json({ message: "El rol es obligatorio" });
+            return res.status(400).json({
+                message: "El rol es obligatorio"
+            })
         }
 
         const nombreLimpio = String(nombre_usuario).trim();
@@ -37,42 +51,83 @@ export const createUser = async (req: Request, res: Response) => {
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(correoLimpio)) {
-            return res.status(400).json({ message: "Formato de correo electrónico no válido" });
+            return res.status(400).json({
+                message: "Formato de correo electrónico no válido"
+            });
         }
 
         const rolExist = await Role.findByPk(rolId);
+
         if (!rolExist) {
-            return res.status(404).json({ message: "El rol especificado no existe" });
+            return res.status(404).json({
+                message: "El rol especificado no existe"
+            });
         }
 
-        if ((rolId === 2 || rolId === 3) && (empresaId === null || isNaN(empresaId))) {
-            return res.status(400).json({ message: "El ID de la empresa es obligatorio para usuarios y admins de empresa" });
+        if (rolId === 2 || rolId === 3) {
+            if (empresaId === null || isNaN(empresaId)) {
+                return res.status(400).json({
+                    message: "El ID de la empresa es obligatorio para usuarios y admins de empresa"
+                });
+            }
         }
 
         if (rolId === 1 && empresaId !== null) {
-            return res.status(400).json({ message: "El admin cluster no debe tener empresa asignada" });
+            return res.status(400).json({
+                message: "El admin cluster no debe tener empresa asignada"
+            });
         }
 
         if (empresaId !== null) {
             const empresa = await Empresa.findByPk(empresaId);
-            if (!empresa) return res.status(404).json({ message: "Empresa no encontrada" });
-            if (!empresa.activo) return res.status(400).json({ message: "La empresa está inactiva" });
+
+            if (!empresa) {
+                return res.status(404).json({
+                    message: "Empresa no encontrada"
+                });
+            }
+
+            if (!empresa.activo) {
+                return res.status(400).json({
+                    message: "La empresa esta inactiva"
+                });
+            }
         }
 
-        const exist = await User.findOne({ where: { correo_electronico: correoLimpio } });
-        if (exist) return res.status(409).json({ message: "Ya existe un usuario con ese correo electrónico" });
+        const exist = await User.findOne({
+            where: { correo_electronico: correoLimpio }
+        });
+
+        if (exist) {
+            return res.status(409).json({
+                message: "Ya existe un usuario con ese correo electronico"
+            });
+        }
 
         const user_log = (req as any).user;
+
         if (user_log.rol_id === 3) {
-            return res.status(403).json({ message: "No autorizado para crear usuarios" });
+            return res.status(403).json({
+                message: "No autorizado para crear usuarios"
+            });
         }
+
         if (user_log.rol_id === 2) {
-            if (rolId !== 3 || empresaId !== user_log.empresa_id) {
-                return res.status(403).json({ message: "No autorizado para crear este tipo de usuario o en otra empresa" });
+            if (rolId !== 3) {
+                return res.status(403).json({
+                    message: "No autorizado para crear ese tipo de usuario"
+                });
+            }
+
+            if (empresaId !== user_log.empresa_id) {
+                return res.status(403).json({
+                    message: "No autorizado para crear usuarios en otra empresa"
+                });
             }
         }
 
         const hashedPassword = await bcrypt.hash(contrasenaLimpia, 10);
+
         const user = await User.create({
             nombre_usuario: nombreLimpio,
             correo_electronico: correoLimpio,
@@ -83,117 +138,232 @@ export const createUser = async (req: Request, res: Response) => {
 
         return res.status(201).json({
             message: "Usuario creado correctamente",
-            user: {
-                id_usuario: user.id_usuario,
-                nombre_usuario: user.nombre_usuario,
-                correo_electronico: user.correo_electronico,
-                rol_id: user.rol_id,
-                empresa_id: user.empresa_id
-            }
+            id_usuario: user.id_usuario,
+            nombre_usuario: user.nombre_usuario,
+            correo_electronico: user.correo_electronico,
+            rol_id: user.rol_id,
+            empresa_id: user.empresa_id
         });
 
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Error al crear usuario" });
+        console.error("Error al crear usuario:", error)
+        return res.status(500).json({
+            message: "Error al crear usuario"
+        });
     }
 };
 
 export const getUsers = async (req: Request, res: Response) => {
+
     try {
-        const users = await User.findAll({
-            attributes: ["id_usuario", "nombre_usuario", "correo_electronico", "rol_id", "empresa_id"],
-            include: [{ model: Role, attributes: ["nombre_rol"] }],
+        const user = await User.findAll({
+            attributes: [
+                "id_usuario",
+                "nombre_usuario",
+                "correo_electronico",
+                "rol_id",
+                "empresa_id",
+            ],
+            include: [
+                {
+                    model: Role,
+                    attributes: ["nombre_rol"]
+                }
+            ],
             order: [["nombre_usuario", "ASC"]]
         });
-        return res.json(users);
+
+        return res.json(user);
+
     } catch (error) {
-        return res.status(500).json({ message: "Error al obtener usuarios" });
+        return res.status(500).json({
+            message: "Error al obtener usuarios"
+        });
     }
 };
 
 export const getUserById = async (req: Request, res: Response) => {
+
     try {
-        const id = req.params.id as string;
+
+        const id = z.uuid().parse(req.params.id);
 
         const user = await User.findByPk(id, {
-            attributes: ["id_usuario", "nombre_usuario", "correo_electronico", "rol_id", "empresa_id"],
-            include: [{ model: Role, attributes: ["nombre_rol"] }]
+            attributes: [
+                "id_usuario",
+                "nombre_usuario",
+                "correo_electronico",
+                "rol_id",
+                "empresa_id"
+            ],
+            include: [
+                {
+                    model: Role,
+                    attributes: ["nombre_rol"]
+                }
+            ]
         });
 
-        if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+        if (!user) {
+            return res.status(404).json({
+                message: "Usuario no encontrado"
+            });
+        }
 
         const user_log = (req as any).user;
-        if (user_log.rol_id === 3 && user_log.id_usuario !== user.id_usuario) {
-            return res.status(403).json({ message: "No autorizado. Solo puedes ver tu propio perfil." });
-        } 
-        if (user_log.rol_id === 2 && user_log.empresa_id !== user.empresa_id) {
-            return res.status(403).json({ message: "No autorizado. Este usuario pertenece a otra empresa." });
+
+        if (user_log.rol_id === 3) {
+            if (user_log.id_usuario !== user.id_usuario) {
+                return res.status(403).json({
+                    message: "No autorizado. Solo puedes ver tu propio perfil."
+                });
+            }
+        } else if (user_log.rol_id === 2) {
+            if (user_log.empresa_id !== user.empresa_id) {
+                return res.status(403).json({
+                    message: "No autorizado. Este usuario pertenece a otra empresa."
+                });
+            }
         }
 
         return res.json(user);
+
     } catch (error) {
-        return res.status(500).json({ message: "Error al obtener usuario" });
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                message: "ID de usuario inválido"
+            });
+        }
+        return res.status(500).json({
+            message: "Error al obtener usuario"
+        });
     }
 };
 
 export const getUsersByEmpresa = async (req: Request, res: Response) => {
     try {
         const empresa_id = Number(req.params.empresa_id);
-        if (isNaN(empresa_id)) return res.status(400).json({ message: "ID de empresa inválido" });
+
+        if (isNaN(empresa_id)) {
+            return res.status(400).json({
+                message: "ID de empresa inválido"
+            });
+        }
 
         const empresa = await Empresa.findByPk(empresa_id);
-        if (!empresa) return res.status(404).json({ message: "Empresa no encontrada" });
+        if (!empresa) {
+            return res.status(404).json({
+                message: "Empresa no encontrada"
+            });
+        }
+
+        if (!empresa.activo) {
+            return res.status(400).json({
+                message: "La empresa esta inactiva"
+            });
+        }
 
         const user_log = (req as any).user;
-        if (user_log.rol_id === 3 || (user_log.rol_id === 2 && user_log.empresa_id !== empresa_id)) {
-            return res.status(403).json({ message: "No autorizado." });
+
+        if (user_log.rol_id === 3) {
+            return res.status(403).json({
+                message: "No autorizado."
+            });
+        } else if (user_log.rol_id === 2) {
+            if (user_log.empresa_id !== empresa_id) {
+                return res.status(403).json({
+                    message: "No autorizado para consultar usuarios de otra empresa."
+                });
+            }
         }
 
         const users = await User.findAll({
             where: { empresa_id },
-            attributes: ["id_usuario", "nombre_usuario", "correo_electronico", "rol_id", "empresa_id"],
-            include: [{ model: Role, attributes: ["nombre_rol"] }],
+            attributes: [
+                "id_usuario",
+                "nombre_usuario",
+                "correo_electronico",
+                "rol_id",
+                "empresa_id"
+            ],
+            include: [
+                {
+                    model: Role,
+                    attributes: ["nombre_rol"]
+                }
+            ],
             order: [["nombre_usuario", "ASC"]]
         });
+
         return res.json(users);
+
     } catch (error) {
-        return res.status(500).json({ message: "Error al obtener los usuarios de la empresa" });
+        console.error("Error en getUsersByEmpresa: ", error);
+        return res.status(500).json({
+            message: "Error al obtener los usuarios de la empresa"
+        });
     }
 };
 
 export const updateUser = async (req: Request, res: Response) => {
+
     try {
-        const id = req.params.id as string;
+        const idUser = z.uuid().parse(req.params.id);
 
-        const user = await User.findByPk(id);
-        if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+        const user = await User.findByPk(idUser);
 
-        const user_log = (req as any).user;
-        if (user_log.rol_id === 3 && user_log.id_usuario !== id) {
-            return res.status(403).json({ message: "No autorizado" });
-        } 
-        if (user_log.rol_id === 2 && user.empresa_id !== user_log.empresa_id) {
-            return res.status(403).json({ message: "No autorizado" });
+        if (!user) {
+            return res.status(404).json({
+                message: "Usuario no encontrado"
+            });
         }
 
-        const allowedFields = ["nombre_usuario", "correo_electronico", "contrasena"];
+        const user_log = (req as any).user;
+
+        if (user_log.rol_id === 3) {
+            if (user_log.id_usuario !== idUser) {
+                return res.status(403).json({
+                    message: "No autorizado"
+                });
+            }
+        } else if (user_log.rol_id === 2) {
+            if (user.empresa_id !== user_log.empresa_id) {
+                return res.status(403).json({
+                    message: "No autorizado"
+                });
+            }
+        }
+
+        const allowedFields = [
+            "nombre_usuario",
+            "correo_electronico",
+            "contrasena"
+        ];
+
         const updates: any = {};
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         for (const key of allowedFields) {
-            if (req.body[key] !== undefined) {
-                const valueLimpio = String(req.body[key]).trim();
-                if (valueLimpio === "") return res.status(400).json({ message: "El campo no puede estar vacío" });
-                
-                if (key === "correo_electronico") {
-                    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                    if (!emailRegex.test(valueLimpio)) return res.status(400).json({ message: "Email inválido" });
-                    
-                    const exist = await User.findOne({ 
-                        where: { correo_electronico: valueLimpio, id_usuario: { [Op.ne]: id } } 
+            const value = req.body[key];
+
+            if (value !== undefined) {
+                const valueLimpio = String(value).trim();
+
+                if (valueLimpio === "") {
+                    return res.status(400).json({
+                        message: "El campo no puede estar vacio"
                     });
-                    if (exist) return res.status(409).json({ message: "Correo ya en uso" });
                 }
-                updates[key] = valueLimpio;
+
+                if (key === "correo_electronico" && !emailRegex.test(valueLimpio)) {
+                    return res.status(400).json({
+                        message: "Formato de correo electrónico no válido"
+                    });
+                }
+
+                updates[key] = key === "correo_electronico" 
+                ? valueLimpio.toLowerCase() 
+                : valueLimpio;
             }
         }
 
@@ -201,37 +371,90 @@ export const updateUser = async (req: Request, res: Response) => {
             updates.contrasena = await bcrypt.hash(updates.contrasena, 10);
         }
 
+        if (updates.correo_electronico) {
+            const exist = await User.findOne({
+                where: {
+                    correo_electronico: updates.correo_electronico,
+                    id_usuario: { [Op.ne]: idUser }
+                }
+            });
+
+            if (exist) {
+                return res.status(409).json({
+                    message: "Correo ya en uso"
+                })
+            }
+        }
+
         await user.update(updates);
-        return res.json({ message: "Usuario actualizado correctamente" });
+
+        return res.json({
+            message: "Usuario actualizado correctamente"
+        });
 
     } catch (error) {
-        return res.status(500).json({ message: "Error al actualizar usuario" });
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                message: "ID de usuario inválido"
+            });
+        }
+        return res.status(500).json({
+            message: "Error al actualizar usuario"
+        });
     }
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id as string;
 
-        const user = await User.findByPk(id);
-        if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+    try {
+
+        const idUser =z.uuid().parse(req.params.id);
+
+        const user = await User.findByPk(idUser);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Usuario no encontrado"
+            });
+        }
 
         if (user.rol_id === 1) {
-            return res.status(403).json({ message: "No se puede eliminar un usuario admin cluster" });
+            return res.status(403).json({
+                message: "No se puede eliminar un usuario admin cluster"
+            });
         }
 
         const user_log = (req as any).user;
-        if (user_log.rol_id === 3 && user_log.id_usuario !== id) {
-            return res.status(403).json({ message: "No autorizado para eliminar a otro usuario" });
-        } 
-        if (user_log.rol_id === 2 && user.empresa_id !== user_log.empresa_id) {
-            return res.status(403).json({ message: "No autorizado para eliminar usuarios de otra empresa" });
+
+        if (user_log.rol_id === 3) {
+            if (user_log.id_usuario !== idUser) {
+                return res.status(403).json({
+                    message: "No autorizado para eliminar a otro usuario"
+                });
+            }
+        } else if (user_log.rol_id === 2) {
+            if (user.empresa_id !== user_log.empresa_id) {
+                return res.status(403).json({
+                    message: "No autorizado para eliminar usuarios de otra empresa"
+                });
+            }
         }
 
         await user.destroy();
-        return res.json({ message: "Usuario eliminado correctamente" });
+
+        return res.json({
+            message: "Usuario eliminado correctamente"
+        });
 
     } catch (error) {
-        return res.status(500).json({ message: "Error al eliminar usuario" });
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                message: "ID de usuario inválido"
+            });
+        }
+        console.error("Error al eliminar usuario: ", error);
+        return res.status(500).json({
+            message: "Error al eliminar usuario"
+        });
     }
 };
